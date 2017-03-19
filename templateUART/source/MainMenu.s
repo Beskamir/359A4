aca1b8dbca953f3fbfc0fee83707e2d012fd7266
Quantumdit <yehonatan.shabash@ucalgary.ca>
2017-03-19 01:21:00 -0600

    Changed "Menu" to MainMenu

--- templateUART/source/MainMenu.s
+++ templateUART/source/MainMenu.s
@@ -1,13 +1,13 @@
 //This file handles the main menu
 
-.globl Menu								//Menu is the only public function in this file
+.globl mainMenu								//Menu is the only public function in this file
 
 .section .text
 
 //input: null
 //return: r0 - 0 if user wants to quit, 1 if user wants to start the game
 //effect: Runs the menu
-Menu:
+MainMenu:
 	push	{r4-r12, fp, lr}			//Push all the general purpose registers along with fp and lr to the stack
 	
 	bl		drawMenu					//Draw the menu
