90f3d7404965a71abe0c7f4697b41ea452ce95aa
SebastianK <sebastian.kopacz@ucalgary.ca>
2017-03-30 16:39:26 -0600

    maybe cores will work?

--- templateUART/source/main.s
+++ templateUART/source/main.s
@@ -24,6 +24,10 @@ _start:
     
 .section .text
 
+_t_core_stack1: .word 0
+_t_core_stack2: .word 0
+_t_core_stack3: .word 0
+
 main:
 
 	//Idea for initing cores, move this code after they've been activated. Also test using print console debugging
@@ -41,13 +45,13 @@ main:
 
 	/// In theory this will stop the cpu's from fighting over resources
 	//and make things faster
-	// ldr r0,=0x4000008C
-	// ldr r1,=_f_core1Init
-	// str r1,[r0,#0x10]
-	// ldr r1,=_f_core2Init
-	// str r1,[r0,#0x20]
-	// ldr r1,=_f_core3Init
-	// str r1,[r0,#0x30]
+	ldr r0,=0x4000008C
+	ldr r1,=_f_core1Init
+	str r1,[r0,#0x10]
+	ldr r1,=_f_core2Init
+	str r1,[r0,#0x20]
+	ldr r1,=_f_core3Init
+	str r1,[r0,#0x30]
 
 	bl _f_enableCache
 
@@ -69,6 +73,7 @@ main:
 ////End of Actual coreLoop
 
 _f_core1Init:
+    ldr	sp, =_t_core_stack1 // set stack pointer for core 1
 
 	//Set up core 1
 	bl _f_enableCache
@@ -83,13 +88,12 @@ _f_core1Init:
 	mrc p15,0,r0,c1,c0,2
 	mov r0,#0x40000000
 	// vmsr fpexc,r0
-    ldr		sp, =_core1_Loop // Initializing the stack pointer
-	// ldr sp,=0xFFF //Must be unique for CPU1
 	_core1_Loop:
 			//core 1 code here
 		b _core1_Loop
 
 _f_core2Init:
+    ldr	sp, =_t_core_stack2 // set stack pointer for core 2
 
 	//Set up core 2
 	bl _f_enableCache
@@ -104,13 +108,13 @@ _f_core2Init:
 	mrc p15,0,r0,c1,c0,2
 	mov r0,#0x40000000
 	// vmsr fpexc,r0
-    ldr		sp, =_core2_Loop // Initializing the stack pointer
-	// ldr sp,=0xFF //Must be unique for CPU2
 	_core2_Loop:
 			//core 2 code here
 		b _core2_Loop
 
 _f_core3Init:
+    ldr	sp, =_t_core_stack3 // set stack pointer for core 3
+
 	//Set up core 3
 	bl _f_enableCache
 	// MRC p15,0,r0,c1,c0,0
@@ -124,8 +128,6 @@ _f_core3Init:
 	mrc p15,0,r0,c1,c0,2
 	mov r0,#0x40000000
 	// vmsr fpexc,r0
-    ldr		sp, =_core3_Loop // Initializing the stack pointer
-	// ldr sp,=0xF //Must be unique for CPU3
 	_core3_Loop:
 			//core 3 code here
 		b _core3_Loop
