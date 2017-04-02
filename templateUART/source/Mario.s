96220698efe787e192a8230ee0c1ef1941e3c740
Quantumdit <yehonatan.shabash@ucalgary.ca>
2017-04-02 01:15:31 -0600

    Mario falling done

--- templateUART/source/Mario.s
+++ templateUART/source/Mario.s
@@ -61,40 +61,50 @@ f_moveMario:
 	sub		r9, #1							//Increase Mario's fall speed by 1
 	strb	r9, [r8]						//Store Mario's new fall speed
 	//Move Mario in map
+	//r9 = Mario's fall speed = loop counter = Number of times Mario is moved down until he hits terrain
 	FallMarioTop:							//Top of the loop, r9 is the loop counter as it is Mario's fall speed
-	lsl		r0, r6, #16						//Move X position to top half of r0
-	orr		r0, r7							//Add Y position to bottom half of r0
-	mov		r1, #0							//Don't move Mario horizontally
-	mov		r2, #-1							//Move Mario 1 space down
-	ldr		r3, =d_mapForeground			//Mario is in the foreground
-	bl		f_moveElement					//Move Mario
-	sub		r9, #1							//Subtract 1 from r9
-	//Loop test
-	cmp		r9, #0							//Compare the loop counter to 0
-	bgt		FallMarioTop					//If r9 is still greater than 0, we need to move Mario down again
-	
-	cmp		
+		lsl		r0, r6, #16					//Move X position to top half of r0
+		orr		r0, r7						//Add Y position to bottom half of r0
+		mov		r1, #0						//Don't move Mario horizontally
+		mov		r2, #-1						//Move Mario 1 space down
+		ldr		r3, =d_mapForeground		//Mario is in the foreground
+		bl		f_moveElement				//Move Mario
+		mov		r10, r0						//Store result in a safe register
+		cmp		r10, #2						//Did the move succeed?
+		moveq	FallMarioTest				//If so, go to the test
+		cmp		r10, #1						//Did we fail to move due to an enemy?
+		bleq	_f_killEnemy				//If so, kill the enemy!
+		beq		FallMarioTest				//Then go to the loop test
+		//If neither, then we're done falling!
+		mov		r9, #0						//Set Mario's vertical speed to 0
+		strb	r9, [r8]					//Store Mario's vertical speed
+		b		doneYMap					//We're done Y movement
+	FallMarioTest:							//Loop test
+		sub		r9, #1						//Subtract 1 from the loop counter
+		sub		r7, #1						//Subtract 1 from Mario's Y position
+		cmp		r9, #0						//Compare the loop counter to 0
+		bgt		FallMarioTop				//If r9 is still greater than 0, we need to move Mario down again
 	//Move Mario in data register
-	sub		r7, #1							//Subtract 1 from Mario's Y position
+	doneYMap:
 	ldr		r8, =_d_marioPositionY			//Load the address of Mario's Y position
 	strh	r7, [r8]						//Store Mario's new Y position
 	b		doneYMovement					//We're done moving Mario vertically
 	
 	//Jumping
 	jumping:
-	//jumping code here
-		//special case: breaking a block
 	
+		//Special case: breaking a block
+			//Special special case: hitting a value block
+		//Special case: hitting an enemy
 	
 	
 	doneYMovement:
-	
 	//X movement
+	//Check if Mario should be moved horizontally
 	cmp		r4, #0							//Compare X offset to 0
 	beq		noXMovement						//If they're equal, branch to skip the X movement
 	bl		_f_shouldMarioMoveX				//Should Mario be moved right now?
 	cmp		r0, #1							//Is the answer yes?
-	bne		noXMovement						//If not, branch to skip the X movement
 	
 
 	//Move Mario in map
@@ -135,25 +145,60 @@ _f_isMarioOnGround:
 //Input: Null	
 //Output: r0 - 1 for yes, 0 for no
 //Effect: Null
-_f_shouldMarioMoveX:
+_f_shouldMarioMove:
 	push	{r4-r10, lr}					//Push all the general purpose registers along with fp and lr to the stack
 
 	ldr		r4, =_d_lastMoveTick			//Load the address of Mario's X last move time
 	ldr		r5, [r4]						//Load Mario last move time
 
-	ldr		r6, =_t_marioXMoveTiming		//Load the address for the delay for Mario's movement
+	ldr		r6, =_t_marioMoveTiming			//Load the address for the delay for Mario's movement
 	ldr		r7, [r6]						//Load the delay
 	add		r5, r7							//Add the delay to the last movement time (for comparison purposes)
 	
-	bl		getClock						//Get the current move time
+	bl		f_getClock						//Get the current move time
 	mov		r8, r0							//Save it in a safe register
 	
 	cmp		r8, r5							//Compare the two times
-	movge	r0, #1							//If enough time has passed, return 1
-	strge	r8, [r6]						//If enough time has passed to move Mario, set this as the last move time
-	movelt	r0, #0							//If enough time hasn't passed, return 0
+	movgt	r0, #1							//If enough time has passed, return 1
+	strgt	r8, [r6]						//If enough time has passed to move Mario, set this as the last move time
+	movele	r0, #0							//If enough time hasn't passed, return 0
 	
 	pop		{r4-r10, pc}					//Return all the previous registers and return
+	
+	
+	
+//Input: Null	
+//Output: Null
+//Effect: Kills an enemy and moves Mario into its place
+_f_killEnemy:
+	push	{r4-r10, lr}					//Push all the general purpose registers along with fp and lr to the stack
+	
+	//Code to move enemy to seperate layer and do stuff with them here
+	
+	//Remove enemy from foreground
+	ldr		r4, =_d_marioPositionX			//Load the address of Mario's X position
+	ldr		r5, =_d_marioPositionY			//Load the address of Mario's Y position
+	ldrh	r0, [r4]						//Load Mario's X position
+	ldrh	r1, [r5]						//Load Mario's Y position
+	add		r1, #1							//Add 1 to get the enemy's Y position below Mario
+	ldr		r2, =d_mapForeground			//Load the foreground's address
+	ldr		r3, #0							//Set an empty cell
+	bl		d_setCellElement				//Set the cell under Mario (the enemy) to a blank
+	
+	
+	//Move Mario
+	ldr		r0, [r4]						//Load Mario's X position
+	lsl		r0, #16							//Move Mario's x position to the top half of the register
+	ldr		r6, [r5]						//Load Mario's Y position
+	orr		r0, r6							//Move Mario's Y position into the bottom half of r0
+	mov		r1, #0							//Don't move Mario horizontally
+	mov		r2, #-1							//Move Mario down one cell
+	ldr		r3, =d_mapForeground			//Load the foreground's address
+	bl		d_moveElement					//Move Mario
+	
+	pop		{r4-r10, pc}					//Return all the previous registers and return
+	
+	
 	
 	
 //Mario's default map coordinates, used to place him at the start of the game
@@ -161,18 +206,18 @@ _t_marioDefaultPosition:
 _t_marioDefaultPositionX:	.hword 2
 _t_marioDefaultPositionY:	.hword 21
 
-//How many ticks of the clock before Mario moves once horizontally
-_t_marioXMoveTiming:		.byte 1
+//Minimum number of clock ticks between movements
+_t_marioMoveTiming:			.byte 1
 	
-.section		.data
+.section					.data
 
 //Jump/Fall state register, stores whether Mario
-_d_verticalState:		.byte 0
+_d_verticalState:			.byte 0
 
 //Last tick in which Mario moved
-_d_lastMoveTick:		.word 0
+_d_lastMoveTick:			.word 0
 
 //Mario's coordinates in the map
 _d_marioPosition:
-_d_marioPositionX:	.hword 2
-_d_marioPositionY:	.hword 21
+_d_marioPositionX:			.hword 2
+_d_marioPositionY:			.hword 21
