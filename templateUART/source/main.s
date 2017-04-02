56ba52a2fdddcaadfab9198ad0192605a02b33da
sebastian.kopacz <sebastian.kopacz@pi04-wb.cs.ucalgary.ca>
2017-04-02 15:45:26 -0600

    cleaned up main

--- templateUART/source/main.s
+++ templateUART/source/main.s
@@ -9,15 +9,8 @@ Optimization:
 	http://www.valvers.com/open-software/raspberry-pi/step05-bare-metal-programming-in-c-pt5/
 */
 
-// .extern	snes
-// .extern UpdateScreen
-// .include "snes"
-
 .section    .init
-// .include "Graphics.s"
-// .include "art.s"
 .globl    	_start
-// .globl 		coreState
 
 _start:	
     b       main
@@ -38,17 +31,6 @@ main:
 	bl 		InitFrameBuffer //Enable Frame Buffer
 	bl		init_GPIO	//Enable the GPIO pins
 
-
-	/// In theory this will stop the cpu's from 
-	//	fighting over resources and make things faster
-	ldr r0,=0x4000008C
-	ldr r1,=_f_core1Init
-	str r1,[r0,#0x10]
-	ldr r1,=_f_core2Init
-	str r1,[r0,#0x20]
-	ldr r1,=_f_core3Init
-	str r1,[r0,#0x30]
-
 	bl _f_enableCache
 
 	//TestCode
@@ -67,33 +49,6 @@ main:
 
 	b _core0_loop
 ////End of Actual coreLoop
-
-_f_core1Init:
-    ldr	sp, =_t_core_stack1 // set stack pointer for core 1
-
-	//Set up core 1
-	bl _f_enableCache
-	_core1_Loop:
-			//core 1 code here
-		b _core1_Loop
-
-_f_core2Init:
-    ldr	sp, =_t_core_stack2 // set stack pointer for core 2
-
-	//Set up core 2
-	bl _f_enableCache
-	_core2_Loop:
-			//core 2 code here
-		b _core2_Loop
-
-_f_core3Init:
-    ldr	sp, =_t_core_stack3 // set stack pointer for core 3
-
-	//Set up core 3
-	bl _f_enableCache
-	_core3_Loop:
-			//core 3 code here
-		b _core3_Loop
 
 
 haltLoop$:	//Halts the program
