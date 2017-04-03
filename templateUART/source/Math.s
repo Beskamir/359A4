0e0e412f64f1a5c943ed55bf53d190cdcd3f1ff4
sebastian.kopacz <sebastian.kopacz@pi03-wb.cs.ucalgary.ca>
2017-04-03 16:07:34 -0600

    value pack should properly be stored in the middle map

--- templateUART/source/Math.s
+++ templateUART/source/Math.s
@@ -5,6 +5,7 @@
 .globl	f_digToASCII
 .globl	f_getClock
 .globl	d_clock
+.globl	f_random
 
 /*Input: 
 	r0, first register to combine
@@ -103,7 +104,83 @@ f_getClock:
 	
 	getClockEnd:
 	pop		{r4-r5, pc}				//Pop the old registers and return
-	
-.section		.data
 
-d_clock:	.word
+/*
+Random Generator Function:
+Input: null
+Output: 
+	r0, the random number generated in range (0 to 31)
+Effect: Null
+*/
+f_random:
+	// Push r4 to r8 ( you need 5 registers for x,y,z,w and t)
+	push {r4-r9, lr}	//Push registers to the stack
+	
+	//r4 = t
+	//r5 = w
+	//r6 = x
+	//r7 = y
+	//r8 = z
+	
+	// load x,y,z,and w integer values from memory
+	ldr	r4, =_d_w
+	ldr	r5, [r4]
+	ldr	r4, =_d_x
+	ldr	r6, [r4]
+	ldr	r4, =_d_y
+	ldr	r7, [r4]
+	ldr	r4, =_d_z
+	ldr	r8, [r4]
+
+	// move x to t
+	mov	r4, r6
+	// xor t by using the following instruction (eor t, t, t, lsl #11)
+	eor	r4, r4, r4, lsl #11
+	// xor it again using 8 instead of 11
+	eor	r4, r4, r4, lsl #8
+	// move y to x
+	mov	r6, r7
+	// move z to y
+	mov	r7, r8
+	// move w to z
+	mov	r8, r5
+	// then xor w (eor t, t, t, lsl #19)
+	eor r5, r4, r4, lsl #19
+	mov r5, r4
+	// xor w with t
+	// then store value of x in its place in memory( do the same for y z and w)
+		// This is made so that they are updated every time and to make it randomly and not every time is based on their initialized
+	
+	mov r9, r5 //store
+	
+	//Shift the values
+	ldr	r4, =_d_w
+	str	r5, [r4]
+	ldr	r4, =_d_x
+	str	r6, [r4]
+	ldr	r4, =_d_y
+	str	r7, [r4]
+	ldr	r4, =_d_z
+	str	r8, [r4]
+
+	mov r0, r9
+	mov r1, #32
+	bl f_modulo
+	// return some value from 0 to 31
+	
+	pop		{r4-r9, pc}	//Pop register from the stack and return
+
+
+.section .data
+
+d_clock: .word
+
+.align 4
+// Create 4 labels in the data section each having an integer value
+// example: x: .int 5000
+// Make sure the number you chose is less than 2^64
+// You should create labels for y, z and w each with different integer value
+_d_w: .int 1605
+_d_x: .int 8201
+_d_y: .int 3955
+_d_z: .int 9602
--- templateUART/source/ValuePack.s
+++ templateUART/source/ValuePack.s
@@ -5,56 +5,57 @@
 .section		.text
 
 
-//Input: Null
-//Output: Random number in r0
-//Effect: Null
-// Random Generator Function (No need for Parameters):
-_f_RNG:
-	// Push r4 to r8 ( you need 5 registers for x,y,z,w and t)
-	push	{r4-r8, lr}	//Push registers to the stack
-	
-	//r4 = t
-	//r5 = w
-	//r6 = x
-	//r7 = y
-	//r8 = z
-	
-	// load x,y,z,and w integer values from memory
-	ldr		r4, =_d_w
-	ldr		r5, [r4]
-	ldr		r4, =_d_x
-	ldr		r6, [r4]
-	ldr		r4, =_d_y
-	ldr		r7, [r4]
-	ldr		r4, =_d_z
-	ldr		r8, [r4]
-	// move x to t
-	mov		r4, r6
-	// xor t by using the following instruction (eor t, t, t, lsl #11)
-	eor		r4, r4, r4, lsl #11
-	// xor it again using 8 instead of 11
-	eor		r4, r4, r4, lsl #8
-	// move y to x
-	mov		r6, r7
-	// move z to y
-	mov		r7, r8
-	// move w to z
-	mov		r8, r5
-	// then xor w (eor t, t, t, lsl #19)
-	// xor w with t
-	// then store value of x in its place in memory( do the same for y z and w)
-		// This is made so that they are updated every time and to make it randomly and not every time is based on their initialized
-	// return w as the returned value
-	
-	pop		{r4-r8, pc}	//Pop register from the stack and return
+f_randomlyDisplayValuePack:
+	push	{r4-r10, lr}	//Push the registers onto the stack
+
+	randomNumberX_r .req r4
+	randomNumberY_r .req r5
+	cameraOffset_r 	.req r6
+
+	mov randomNumberY_r, #21
+
+	bl f_random
+	mov randomNumberX_r, r0
+
+	ldr cameraOffset_r, =d_cameraPosition
+	ldr cameraOffset_r, [cameraOffset_r]
+
+	//compute convert screen based random value into a map based value
+	add randomNumberX_r, cameraOffset_r
+
+	//check that the value pack isn't in something else
+	ensureValuePackIsAccessable:
+		sub randomNumberY_r, #1
+
+		mov r0, randomNumberX_r
+		mov r1, randomNumberY_r
+		ldr r2, =d_mapForeground
+		mov r3, #0
+		f_getCellElement
+		cmp r0, #0
+		bne ensureValuePackIsAccessable
+
+		mov r0, randomNumberX_r
+		mov r1, randomNumberY_r
+		ldr r2, =d_mapMiddleground
+		mov r3, #0
+		f_getCellElement
+		cmp r0, #0
+		bne ensureValuePackIsAccessable
+
+	//store the value pack in the middle map for the next frame
+	mov r0, randomNumberX_r
+	mov r1, randomNumberY_r
+	ldr r2, =d_mapMiddleground
+	mov r3, #110
+	f_setCellElement
+
+	.unreq randomNumberX_r
+	.unreq randomNumberY_r
+	.unreq cameraOffset_r
+
+	pop		{r4-r10, pc}				//Pop the old registers and return
+
 
 .section		.data
 
-// Create 4 labels in the data section each having an integer value
-	// example: x: .int 5000
-	// Make sure the number you chose is less than 2^64
-	// You should create labels for y, z and w each with different integer value
-	_d_w:		.int 1605
-	_d_x:		.int 8201
-	_d_y:		.int 3955
-	_d_z:		.int 9602
